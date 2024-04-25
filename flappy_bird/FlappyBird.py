b34b9aa08c7bbe017a0b03deb9b8a36d226fcfe4
trainslmao <165353756+trainslmao@users.noreply.github.com>
2024-04-25 13:07:25 -0700

    changed repos cuz im dumb
    yup

--- flappy_bird/FlappyBird.py
+++ flappy_bird/FlappyBird.py
@@ -8,19 +8,22 @@ class Bird(pygame.sprite.Sprite):
 
     # class variables
     COLOR = (0, 0, 0)
-    JUMP_HEIGHT = -1
+    JUMP_HEIGHT = -6
+    SIZE = 50
+    GRAVITY = 0.2
 
     def __init__(self, x, y):
-        self.rect = pygame.Rect(x, y, 40, 40)
+        self.rect = pygame.Rect(x, y, self.SIZE, self.SIZE)
         self.y_velocity = 0
+        self.image = pygame.image.load("images/flappybird.jpg")
         self.mask = None
     
     def jump(self):
-        self.y_velocity += self.JUMP_HEIGHT
+        self.y_velocity = self.JUMP_HEIGHT
     
     def fall(self):
         self.rect.y += self.y_velocity
-        self.y_velocity += 0.1
+        self.y_velocity += self.GRAVITY
 
     def draw(self, window):
         pygame.draw.rect(window, self.COLOR, self.rect)
@@ -39,7 +42,17 @@ bird = Bird(50, 50)
 window = pygame.display.set_mode((640, 480))
 
 
+
+
+
 def main(window):
+
+    # initalize variables
+    cooldown = 0
+
+    # game settings
+    JUMP_COOLDOWN = 15
+
     clock = pygame.time.Clock()
     while True:
         clock.tick(60)
@@ -48,6 +61,11 @@ def main(window):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and cooldown == 0:
+                    bird.jump()
+                    cooldown = JUMP_COOLDOWN
         
         window.fill(COLOR)  
 
@@ -56,6 +74,9 @@ def main(window):
         bird.draw(window)
 
         pygame.display.update()
+
+        if cooldown > 0 :
+            cooldown -= 1
 
 
 
