dfdf40d6809a3f636e1643810611780123df7c13
trainslmao <165353756+trainslmao@users.noreply.github.com>
2024-04-25 13:08:56 -0700

    a dumb
    fd

--- flappy_bird/FlappyBird.py
+++ flappy_bird/FlappyBird.py
@@ -9,7 +9,7 @@ class Bird(pygame.sprite.Sprite):
     # class variables
     COLOR = (0, 0, 0)
     JUMP_HEIGHT = -6
-    SIZE = 50
+    SIZE = 30
     GRAVITY = 0.2
 
     def __init__(self, x, y):
@@ -27,31 +27,61 @@ class Bird(pygame.sprite.Sprite):
 
     def draw(self, window):
         pygame.draw.rect(window, self.COLOR, self.rect)
+
+    def getYValue(self):
+        return self.rect.y
     
+    def setYValue(self, yVal):
+        self.rect.y = yVal
+
+    def setYVelocity(self, yVelo):
+        self.y_velocity = yVelo
+    
+class Pipes(pygame.sprite.Sprite):
+    # class variables
+    COLOR = (0, 225, 0)
+    GAP = 50
+    WIDTH = 30
+
+    def __init__(self, x):
+        self.rect = pygame.Rect(x, 0, (x, 0, self.WIDTH, self.GAP))
+        self.mask = None
+    
+    def draw(self, window):
+        pygame.draw.rect(window, self.COLOR, self.rect)
 
 
 
-COLOR = (255, 255, 255)
-
-pygame.init()
-
-pygame.display.set_caption("flappy bird")
-
-bird = Bird(50, 50)
-
-window = pygame.display.set_mode((640, 480))
 
 
 
 
+def main():
 
-def main(window):
+    # settings
+
+    COLOR = (255, 255, 255)
+    WINDOW_HEIGHT = 480
+    WINDOW_LENGTH = 780
+    JUMP_COOLDOWN = 1
 
     # initalize variables
     cooldown = 0
+    start = False
+    game = True
 
-    # game settings
-    JUMP_COOLDOWN = 15
+    pygame.init()
+    pygame.font.init()
+
+    pygame.display.set_caption("flappy bird")
+
+    bird = Bird(50, 150)
+    #pipe = Pipes(400)
+
+    window = pygame.display.set_mode((WINDOW_LENGTH, WINDOW_HEIGHT))
+
+
+    
 
     clock = pygame.time.Clock()
     while True:
@@ -63,14 +93,32 @@ def main(window):
                 sys.exit()
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and cooldown == 0:
-                    bird.jump()
-                    cooldown = JUMP_COOLDOWN
+                if event.key == pygame.K_SPACE and cooldown == 0 and bird.getYValue() > 0 and game == True:
+                    if start != True:
+                        bird.jump()
+                        start = True
+                        cooldown = JUMP_COOLDOWN
+                    
+                    else:
+                        bird.jump()
+                        cooldown = JUMP_COOLDOWN
         
         window.fill(COLOR)  
 
-        bird.fall()
+        # check game conditions
+        if start:
+            bird.fall()
+
+        if bird.getYValue() < 0:
+            bird.setYValue(0)
         
+        if bird.getYValue() > WINDOW_HEIGHT:
+            bird.setYValue(WINDOW_HEIGHT - 10)
+            font = pygame.font.SysFont("Comic Sans MS", 50)
+            text_surface = font.render('GAME OVER', False, (255, 0, 0))
+            window.blit(text_surface, (WINDOW_LENGTH / 3, WINDOW_HEIGHT / 2.5))
+
+        #pipe.draw(window)
         bird.draw(window)
 
         pygame.display.update()
@@ -81,4 +129,5 @@ def main(window):
 
 
 if __name__ == "__main__":
-    main(window)
+
+    main()
